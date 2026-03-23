c51d3ba7ee1948e6118af4104f8e6bfbb2a1cf04
Dinesh Tamang <dineshtamang7263@gmail.com>
2026-03-24 08:15:15 +0900

    Add Tailwind CSS configuration

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/**
+ * Tailwind CSS configuration file
+ */
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1d4ed8',
+        secondary: '#64748b',
+      },
+    },
+  },
+};
